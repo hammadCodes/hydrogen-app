e4117d3b98089f904444450dae8cf0990223eba8
hammadcodes <engr.hammad.uet@gmail.com>
2022-06-24 13:45:35 +0500

    Version 1.0

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'hammadcodes.myshopify.com',
+    storefrontToken: '648f58110aa687b2a4b26a065058da63',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
